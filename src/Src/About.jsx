8010e78ff38c6b331e39ad980931b17f0e1b5f79
yasir-graphics <myasir8251@gmail.com>
2026-04-14 13:05:38 +0300

    Delete src/Src directory

--- src/Src/About.jsx
+++
@@ -1,8 +0,0 @@
-export default function About(){
-  return (
-    <div style={{padding:50,textAlign:'center'}}>
-      <h2>About Us</h2>
-      <p>We create premium branding and designs for businesses.</p>
-    </div>
-  )
-}
