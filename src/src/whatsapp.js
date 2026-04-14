c8ab1619c2db196c4110200d86b9d79ae908e472
yasir-graphics <myasir8251@gmail.com>
2026-04-14 13:05:56 +0300

    Delete src/src directory

--- src/src/whatsapp.js
+++
@@ -1,6 +0,0 @@
-export const whatsappNumber = '966579878473'
-
-export const sendWhatsApp = (form) => {
-  const text = `Hello, I am ${form.name}%0AEmail: ${form.email}%0AMessage: ${form.message}`
-  window.open(`https://wa.me/${whatsappNumber}?text=${text}`, '_blank')
-}
